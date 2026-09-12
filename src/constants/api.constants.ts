4189657f5ebdd65e69220f6ea7cc344e505c549f
Алиса <alisia.bajeeva247@gmail.com>
2026-09-12 17:28:56 +0500

    k

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:4200/api';
+export const API_URL = 'https://youtube-backend-gm1x.onrender.com';
 export const IS_CLIENT = typeof window != 'undefined';
 export const SITE_URL="http://localhost:3000";
