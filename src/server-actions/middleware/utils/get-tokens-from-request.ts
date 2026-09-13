c8aaeef0c04cc99463b41bdd66e7c04a791d82b7
Алиса <alisia.bajeeva247@gmail.com>
2026-09-13 12:17:21 +0500

    k

--- src/server-actions/middleware/utils/get-tokens-from-request.ts
+++ src/server-actions/middleware/utils/get-tokens-from-request.ts
@@ -1,32 +1,79 @@
-import { EnumTokens } from "@/constants/auth.constants";
-import { authService } from "@/services/auth.services";
-import { AxiosError } from "axios";
-import type { NextRequest } from "next/server";
+// import { EnumTokens } from "@/constants/auth.constants";
+// import { authService } from "@/services/auth.services";
+// import { AxiosError } from "axios";
+// import type { NextRequest } from "next/server";
 
+
+// export async function getTokensFromRequest(request: NextRequest) {
+//   const refreshToken = request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value;
+//   let accessToken = request.cookies.get(EnumTokens.ACCESS_TOKEN)?.value;
+//   console.log('ACCESS TOKEN EXISTS:', !!accessToken)
+//   console.log('REFRESH TOKEN EXISTS:', !!refreshToken)
+//   if (!refreshToken) {
+//     request.cookies.delete(EnumTokens.ACCESS_TOKEN);
+//     return null;
+//   }
+
+//   if (!accessToken) {
+//     try {
+//       const data = await authService.getNewTokensByRefresh(refreshToken);
+//       accessToken = data.accessToken;
+//     } catch (error) {
+//       if (error instanceof AxiosError) {
+//         if (error.message == 'invalid token') {
+//           console.log('Невалидный токен');
+//           request.cookies.delete(EnumTokens.ACCESS_TOKEN);
+//           return null;
+//         }
+//       }
+//       return null;
+//     }
+//   }
+//   return { accessToken, refreshToken };
+// }
+
+import { EnumTokens } from '@/constants/auth.constants'
+import { authService } from '@/services/auth.services'
+import { AxiosError } from 'axios'
+import type { NextRequest } from 'next/server'
 
 export async function getTokensFromRequest(request: NextRequest) {
-  const refreshToken = request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value;
-  let accessToken = request.cookies.get(EnumTokens.ACCESS_TOKEN)?.value;
+  const refreshToken = request.cookies.get(
+    EnumTokens.REFRESH_TOKEN
+  )?.value
+
+  let accessToken = request.cookies.get(
+    EnumTokens.ACCESS_TOKEN
+  )?.value
+
+  console.log('ACCESS TOKEN EXISTS:', !!accessToken)
+  console.log('REFRESH TOKEN EXISTS:', !!refreshToken)
 
   if (!refreshToken) {
-    request.cookies.delete(EnumTokens.ACCESS_TOKEN);
-    return null;
+    console.log('REFRESH TOKEN NOT FOUND')
+    request.cookies.delete(EnumTokens.ACCESS_TOKEN)
+    return null
   }
 
   if (!accessToken) {
     try {
-      const data = await authService.getNewTokensByRefresh(refreshToken);
-      accessToken = data.accessToken;
+      const data = await authService.getNewTokensByRefresh(refreshToken)
+
+      accessToken = data.accessToken
     } catch (error) {
+      console.log('REFRESH ERROR:', error)
+
       if (error instanceof AxiosError) {
-        if (error.message == 'invalid token') {
-          console.log('Невалидный токен');
-          request.cookies.delete(EnumTokens.ACCESS_TOKEN);
-          return null;
+        if (error.message === 'invalid token') {
+          console.log('Невалидный токен')
+          request.cookies.delete(EnumTokens.ACCESS_TOKEN)
+          return null
         }
       }
-      return null;
+
+      return null
     }
   }
-  return { accessToken, refreshToken };
+
+  return { accessToken, refreshToken }
 }
